98efc9d5f49e0c58f51ef00b54394881214f67af
Joe Neuman <Joe Neuman>
2015-12-16 20:03:33 -0700

    "working"

--- controller.js
+++ controller.js
@@ -1,13 +1,27 @@
-var app = angular.module('userProfiles');
+angular.module('userProfiles');
+app.controller('MainController', function ($scope, mainService) {
 
-app.controller('MainController', function($scope, mainService) {
-
-	$scope.getUsers = function () {
-		mainService.getUsers().then(function (response) {
-			// $scope.users = mainService.getUsers();
-			$scope.users = response.data.data;
+	$scope.currentPage = 1;
+	$scope.totalPages = 0;
+	///////////////////
+	$scope.getUsers = function (page) {
+		mainService.getUsers(page).then(function (response) {
+			$scope.users = response.data;
+			$scope.totalPages = response.total_pages;
 		});
-	}	
-  $scope.getUsers();
-
+	};
+	///////////////////
+	$scope.getUsers(1);
+	///////////////////  
+	$scope.previous = function () {
+		$scope.currentPage -= 1;
+		$scope.getUsers($scope.currentPage);
+    };
+	///////////////////
+    $scope.next = function () {
+        $scope.currentPage += 1;
+		$scope.getUsers($scope.currentPage);
+    };
+	///////////////////
 });
+
